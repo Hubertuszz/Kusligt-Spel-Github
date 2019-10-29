1455e1943930d1ce03d422f2758fe53b88357264
Hugo Mattsson <hugo.mattsson@elev.ga.klaragymnasium.se>
2019-10-29 21:46:04 +0100

    Ranked och Quickplay done

---
+++ Escape the infected/Assets/Scripts/Photon/Multiplayer/MultiplayerSettings.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MultiplayerSettings : MonoBehaviour
+{
+    public static MultiplayerSettings mps;
+
+    public bool ranked;
+    public bool customgame;
+    
+
+    private void Awake()
+    {
+        if(MultiplayerSettings.mps == null)
+        {
+            MultiplayerSettings.mps = this;
+        }
+        else
+        {
+            if(MultiplayerSettings.mps != this)
+            {
+                Destroy(this.gameObject);
+            }
+        }
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+}
+
---
+++ Escape the infected/Assets/Scripts/Photon/Multiplayer/RoomController.cs
@@ -0,0 +1,53 @@
+﻿using Photon.Pun;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class RoomController : MonoBehaviourPunCallbacks
+{
+    public static RoomController rm;
+    [SerializeField]
+    private int multiplayerSceneIndex;
+    [SerializeField]
+    private int waitingRoomSceneIndex;
+
+
+    
+
+    public override void OnEnable()
+    {
+        PhotonNetwork.AddCallbackTarget(this);
+    }
+
+    public override void OnDisable()
+    {
+        PhotonNetwork.RemoveCallbackTarget(this);
+    }
+
+    public override void OnJoinedRoom()
+    {
+        Debug.Log("Joined Room");
+        if (MultiplayerSettings.mps.ranked)
+        {
+            SceneManager.LoadScene(waitingRoomSceneIndex);
+        }
+        else
+            StartGame();
+    }
+
+    private void StartGame()
+    {
+        if (PhotonNetwork.IsMasterClient)
+        {
+            Debug.Log("Starting Game");
+            PhotonNetwork.LoadLevel(multiplayerSceneIndex);
+        }
+    }
+
+    
+
+    
+
+}
+   
