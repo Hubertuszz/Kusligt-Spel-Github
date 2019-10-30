ae0dffeaae420b85fa97d4bf4e18e1ceee027a51
Hugo Mattsson <hugo.mattsson@elev.ga.klaragymnasium.se>
2019-10-30 16:00:23 +0100

    Typ movement och all multiplayer är klar

--- Escape the infected/Assets/Scripts/Photon/Multiplayer/GameSetupController.cs
+++ Escape the infected/Assets/Scripts/Photon/Multiplayer/GameSetupController.cs
@@ -1,6 +1,8 @@
 ﻿using Photon.Pun;
+using System.Collections;
 using System.IO;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameSetupController : MonoBehaviour
 {
@@ -14,5 +16,33 @@ public class GameSetupController : MonoBehaviour
     {
         Debug.Log("Creating Player");
         PhotonNetwork.Instantiate(Path.Combine("Prefabs", "NetworkPlayer"), Vector3.zero, Quaternion.identity);
+    }
+
+    public static GameSetupController gs;
+
+    public Transform[] spawnPoints;
+
+
+    private void OnEnable()
+    {
+        if (GameSetupController.gs == null)
+        {
+            GameSetupController.gs = this;
+        }
+    }
+
+    public void DisconnectPlayer()
+    {
+        StartCoroutine(DisconnectAndLoad());
+    }
+
+    IEnumerator DisconnectAndLoad()
+    {
+        PhotonNetwork.Disconnect();
+        while (PhotonNetwork.IsConnected)
+        {
+            yield return null;
+            SceneManager.LoadScene(0);
+        }
     }
 }
---
+++ Escape the infected/Assets/Scripts/Player/AvatarSetup.cs
@@ -0,0 +1,38 @@
+﻿using Photon.Pun;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AvatarSetup : MonoBehaviour
+{
+    private PhotonView pv;
+    public int characterValue;
+    public GameObject myCharacter;
+
+    public int playerHealth;
+    public int playerDamge;
+
+    public Camera myCamera;
+    public AudioListener myAL;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        pv = GetComponent<PhotonView>();
+        if (pv.IsMine)
+        {
+            pv.RPC("RPC_AddCharacter", RpcTarget.AllBuffered, PlayerInfo.pi.selectedCharacter);
+        }
+        else
+        {
+            Destroy(myCamera);
+            Destroy(myAL);
+        }
+    }
+
+    [PunRPC]
+    void RPC_AddCharacter(int randomCharacter)
+    {
+        myCharacter = Instantiate(PlayerInfo.pi.allCharacters[randomCharacter], transform.position, transform.rotation, transform);
+    }
+}
